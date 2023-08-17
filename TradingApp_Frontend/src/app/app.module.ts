a79c633084016255259898062632c81eac075de0
AvinashG <avinashgdragonz@gmail.com>
2023-08-17 07:15:09 +0000

    Added home component

--- TradingApp_Frontend/src/app/app.module.ts
+++ TradingApp_Frontend/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
+const routes: Routes = [
+  {path: "", component: HomePageComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
